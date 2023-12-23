79a39c22cb93b3d906082e83069bf7eee8e9f7c3
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-23 15:45:58 +0530

    Added redux persist

--- SBMRS/src/main.jsx
+++ SBMRS/src/main.jsx
@@ -5,9 +5,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider} from 'react-redux'
 import { store} from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor} from './redux/store.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null} >
     <App />
+    </PersistGate>
   </Provider>,
 )
